8af278fe5374b26a7287ab843b9a11aa4ad38475
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-05-17 23:04:48 +0200

    Added tabs.

--- website/src/main/website/app/commons/social-bar.component.ts
+++ website/src/main/website/app/commons/social-bar.component.ts
@@ -26,7 +26,7 @@ import {FacebookFollowButtonComponent} from './facebook-follow-button.component'
 <tweet-button></tweet-button><br/>
 <google-plus-addone-button></google-plus-addone-button>
 <google-plus-follow-button></google-plus-follow-button>
-<google-plus-share-button></google-plus-share-button>
+<google-plus-share-button></google-plus-share-button><br/>
 <facebook-like-button></facebook-like-button>
 <facebook-follow-button></facebook-follow-button>`
 })
