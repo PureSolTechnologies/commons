c3f1f12162f5ddd40453e9d35c1572b0c3ab6da9
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2017-12-20 20:56:37 +0100

    Added new interface.

---
+++ types/src/main/java/com/puresoltechnologies/commons/types/Counter.java
@@ -0,0 +1,24 @@
+package com.puresoltechnologies.commons.types;
+
+/**
+ * This is a special interface to be implemented by classes which implement a
+ * counting functionality like streams counting bytes, iterators counting
+ * elements or statistis classes counting samples.
+ * 
+ * @author Rick-Rainer Ludwig
+ * @param <T>
+ *            is the type of the counter. It makes most sense to use an integer
+ *            type like {@link Byte}, {@link Short}, {@link Integer} or
+ *            {@link Long}.
+ */
+public interface Counter<T extends Number> {
+
+    /**
+     * This method returns the current count. <code>null</code> may be returned in
+     * case the class was not initialized, yet.
+     * 
+     * @return An object of T is returned.
+     */
+    public T getCount();
+
+}
