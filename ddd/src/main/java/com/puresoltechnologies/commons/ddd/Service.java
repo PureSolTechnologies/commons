5ac856a58974eaed4fc917c42cb0acc4ee108f88
Rick-Rainer Ludwig <rick-rainer.ludwig@senorics.com>
2019-07-30 17:28:52 +0200

    Minor doc enhancement.

--- ddd/src/main/java/com/puresoltechnologies/commons/ddd/Service.java
+++ ddd/src/main/java/com/puresoltechnologies/commons/ddd/Service.java
@@ -3,7 +3,10 @@ package com.puresoltechnologies.commons.ddd;
 /**
  * In DDD a service encapsulates operations which do not belong to
  * {@link ValueObject} or {@link Entity} objects.
- * 
+ * <p>
+ * As services do not have pre-defined functionality, this interface is an empty
+ * marker interface.
+ *
  * @author Rick-Rainer Ludwig
  */
 public interface Service extends DDDElement {
